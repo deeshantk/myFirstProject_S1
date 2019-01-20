a19349d89df93f1e5f0f35df392b63f6f5e48e87
schankanyal <30956347+schankanyal@users.noreply.github.com>
2019-01-20 11:58:04 +0530

    added sum functionality

--- test_file.c
+++ test_file.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-
+int a=4,b=8;
 printf("Hello World!");
 
 //if printed successfully, test is successful!
 
 printf("SUCCESS.")
-  
+  c=a+b;
+print("%d",c);
 return 0;
 }
